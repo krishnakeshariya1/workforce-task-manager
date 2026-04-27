2cc382d6f4b47d243dfc0aa0e6cd5134f0a08a11
krishnakeshariya1 <keshariyakrishna8@gmail.com>
2026-04-27 18:45:58 +0530

    resolve bug

--- workforce_task_manager/src/Pages/Admin/EmployeesDetail.jsx
+++ workforce_task_manager/src/Pages/Admin/EmployeesDetail.jsx
@@ -1,12 +1,29 @@
 import { Outlet } from "react-router-dom"
 import { Navbar } from "../../Component/Navbar"
 import { CreateEmployeeForm } from "../../Component/CreateEmployeeForm"
+import { useEffect, useState } from "react"
+import { getData } from "../../Utils/localStorage"
 
 export const EmployeesDetail = () =>{
+
+    const [users, setUsers] = useState([]);
+
+    useEffect(()=>{
+        const appData = getData();
+        setUsers(appData.users)
+    },[])
+
     return(
-        <div className="w-full h-auto  bg-[var(--body-background-Color)] flex flex-col gap-10 ">
-            <div className=" grid grid-cols-[30%_69%] text-2xl text-white justify-center gap-2 ">
-                <h1 className="text-center border-2 border-amber-50">krishna </h1>
+        <div className="w-full h-auto  bg-[var(--body-background-Color)] flex flex-col">
+            <div className=" grid grid-cols-[auto_auto] text-2xl justify-start gap-8 bg-[var(--navbar-color)] py-2 px-8">
+                <div className="flex flex-col gap-1 bg-gray-200 py-4 px-8 rounded-2xl">
+                    <h1 className="text-2xl text-[var(--primary-button-Color)] font-semibold mb-5">All Employee Info :</h1>
+                    {users.map((user)=>{
+                        return <h1 key={user.id}>
+                            {user.name}
+                        </h1>
+                    } )}
+                </div>
                 <h2 className="text-start border-2 border-amber-50 ">keshariya</h2>
             </div>
            
